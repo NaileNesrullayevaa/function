4f86daaa0d5b9785354bc4449ee2f22536af05ed
NaileNesrullayevaa <nailenasrullayeva536@gmail.com>
2024-11-14 10:56:05 -0800

    'first'

---
+++ main.js
@@ -0,0 +1,63 @@
+3// const sentence="Salam hormetli "
+// const name="Naile"
+// function test(sentence){
+//     return  sentence + name
+// }
+// console.log(test(sentence));
+
+
+
+// function test(n){
+//     if(n%2==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+// console.log(test(25));
+
+
+
+
+// function test(name,height,phone,hobbies){
+//     result=`name: ${name} height: ${height} phone: ${phone} hobbies: ${hobbies}`
+//     return result
+// }
+// console.log(test('Cavad',185,{model:'Samsung',version:'Galaxy'},['reading','coding']));
+
+
+
+
+// let cub=(reqem)=>{
+//     return reqem*reqem*reqem
+// }
+// console.log(cub(3));
+
+
+
+
+// function toCelcius(fahrenheit){
+//     return (fahrenheit-32) / 1.8
+// }
+// console.log(toCelcius(77));
+
+// function toCelcius(fahrenheit){
+//     result1 =(fahrenheit-32) /1.8
+//     result2=100-fahrenheit
+//     result="result1 + result2"
+//     return result
+// }
+// console.log(toCelcius(77));
+
+
+
+
+
+function reverse(number){
+    result=
+}
+console.log(reverse(123));
+
+
+
